5993828e23f22b80a2166f90756f2f3ae94ae4e6
Nick <nbumbarger@gmail.com>
2015-06-05 09:48:02 -0400

    Parity with project shown in demo video; needs work before ready for public consumption

--- app/assets/javascripts/media_contents.js
+++ app/assets/javascripts/media_contents.js
@@ -1,51 +1,93 @@
-$(function() {
-  var mediaDropzone;
-  mediaDropzone = new Dropzone("#media-dropzone");
-  mediaDropzone.on("addedfile", function(file) {
-    getCoords(file, function(coords) {
-    $('#lat').val(coords.lat);
-    $('#lng').val(coords.lng)
-    // alert("added to hidden field: "+coords.lat+" "+coords.lng )
-    })
-  });
-  mediaDropzone.on("sending", function(file, xhr, formData) {
-    getCoords(file, function(coords) {
-    // alert("attempting to append to formData: "+coords.lat+" "+coords.lng )
-    formData.append("coords", coords);
-    })
-  });
-  mediaDropzone.on("success", function(file, responseText) {
-  // alert("This event should fire last")
-  appendImage(responseText.file.url, responseText.id);
-  setTimeout(function(){
-    $(file.previewElement).fadeOut(2000)
-    }, 1000);
-  });
-});
+function initMain(mapboxToken, geoJSON) {
 
-function exifCoordToDec(exifCoord) {
-  var degreeDec = exifCoord[0].numerator
-  var minuteDec = exifCoord[1].numerator / (60 * exifCoord[1].denominator)
-  var secondDec =  exifCoord[2].numerator / (3600 * exifCoord[2].denominator)
-  return degreeDec + minuteDec + secondDec
-};
-
-function getCoords(image, callback) {
-  EXIF.getData(image, function() {
-    var lng = EXIF.getTag(this, 'GPSLongitude');
-    var lat = EXIF.getTag(this, 'GPSLatitude');
-    var lngDec = exifCoordToDec(lng)
-    var latDec = exifCoordToDec(lat)
-    callback({lat:latDec, lng:lngDec})
+  $(function() {
+    var mediaDropzone;
+    // Initialize upload dropzone
+    mediaDropzone = new Dropzone("#media-dropzone");
+    // When file is dropped, extract coordinates from image and convert to DD
+    mediaDropzone.on("addedfile", function(file) {
+      getCoords(file, function(coords) {
+      // Store converted lat/long in hidden fields
+      $('#lat').val(coords.lat);
+      $('#lng').val(coords.lng)
+      })
+    });
+    // When upload succeeds...
+    mediaDropzone.on("success", function(file, responseText) {
+      // ...append image to image manager
+      appendImage(responseText.file.url, responseText.id);
+      setTimeout(function(){
+        // reload JSON into the map
+        loadFeatures();
+        // Remove upload status files
+        $(file.previewElement).fadeOut(2000)
+        }, 1000);
+    });
   });
-};
 
-var appendImage = function(imageUrl, mediaId) {
-  var imageMarkup = '<img src="' + imageUrl + '"/>' +
-    '<input id="media_contents_" name="media_contents[]" value="' + mediaId +'" type="checkbox" class="checkbox">'
-  $(imageMarkup).hide().appendTo(".saved-images").fadeIn(1500)
-};
+  // Convert a coordinate in degrees, minutes seconds format to decimal
+  function exifCoordToDec(exifCoord) {
+    var degreeDec = exifCoord[0].numerator
+    var minuteDec = exifCoord[1].numerator / (60 * exifCoord[1].denominator)
+    var secondDec =  exifCoord[2].numerator / (3600 * exifCoord[2].denominator)
+    return degreeDec + minuteDec + secondDec
+  };
+
+  // Get coordinates from the GPSLat and Long EXIF attributes of the image
+  function getCoords(image, callback) {
+    EXIF.getData(image, function() {
+      var lng = EXIF.getTag(this, 'GPSLongitude');
+      var lat = EXIF.getTag(this, 'GPSLatitude');
+      var lngDec = exifCoordToDec(lng)
+      var latDec = exifCoordToDec(lat)
+      callback({lat:latDec, lng:lngDec})
+    });
+  };
+
+  // Append uploaded images to image manager 
+  var appendImage = function(imageUrl, mediaId) {
+    var imageMarkup = '<img src="' + imageUrl + '"/>' +
+      '<input id="media_contents_" name="media_contents[]" value="' + mediaId +'" type="checkbox" class="checkbox">'
+    $(imageMarkup).hide().appendTo(".saved-images").fadeIn(1500)
+  };
   
+  L.mapbox.accessToken = mapboxToken;
+  // Initialize map frame with default zoom behavior disabled
+  var map = L.mapbox.map('map', 'mapbox.streets', {doubleClickZoom: false})
+    // Add custom zoom behavior (pan to center of double-click event)
+    .on('dblclick', function(e) {
+      map.setView(e.latlng, map.getZoom() + 1)
+    });
+  // Add empty feature layer to map frame
+  var featureLayer = L.mapbox.featureLayer()
+    featureLayer.on('layeradd', function(e){
+      addPopups(e)
+    })
+    .addTo(map)
+
+  // Load features from GeoJSON server
+  function loadFeatures() {
+    $.ajax({
+      type: 'GET',
+      dataType: 'json',
+      url: geoJSON})
+    .done(function (geojson) { 
+      // Load response GeoJSON into feature layer
+      featureLayer.setGeoJSON(geojson);
+      // Fit map to bounding coordinates of features
+      map.fitBounds(featureLayer.getBounds(), map.getZoom() - 10);
+    });
+  };
+
+  // Add HTML to map popup baloons
+  function addPopups(e) {
+      var marker = e.layer;
+      var feature = marker.feature;
+        marker.bindPopup(feature.properties.popup_html, {
+          closeButton: false
+    });
+  };
+}
 
 // getCoords (HTTP Request version). I may need this later.
 // function getCoords(imageUrl) {
