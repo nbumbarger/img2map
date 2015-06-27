f2fa6a367a2cfc12c7ec0b42fcee2516037b8edb
Nick Bumbarger <nbumbarger@gmail.com>
2015-06-27 13:04:33 -0400

    more cloudinary configuration

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,10 +4,10 @@ class ImageUploader < CarrierWave::Uploader::Base
     include Cloudinary::CarrierWave
   end
 
-  storage :file
+  # storage :file
 
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
+  # def store_dir
+  #   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  # end
 
 end
