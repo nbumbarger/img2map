6d259d0a5903a6e5cfda2f1254e0c1d4bbffacd8
Nick Bumbarger <nbumbarger@gmail.com>
2015-06-28 01:06:38 -0400

    Disabled post-compile

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,5 +1,5 @@
 class ImageUploader < CarrierWave::Uploader::Base
-	
+
   if Rails.env.production?
     include Cloudinary::CarrierWave
   end
@@ -7,7 +7,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   if Rails.env.development?
   	storage :file
     def store_dir
-      "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+      'uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}'
     end
   end
 
